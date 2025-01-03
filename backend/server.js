b02761fcb8f1bdd8f81b4712746e9d994090cb99
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-01-03 23:34:53 +0530

    add dotenv support, update server script, and implement auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
-import express from 'express';
-
+import express from 'express'
+import dotenv from 'dotenv' ;
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 const app = express()   
- app.listen(5000, () => {
-    console.log('server started on http://localhost:5000');
+
+app.get('/', (req, res) => {
+      res.send('radha radha')
+})
+
+app.use("api/auth",authRoutes);
+
+
+ app.listen(PORT  , () => {
+    console.log(`server started on http://localhost:${PORT}`);
  })
