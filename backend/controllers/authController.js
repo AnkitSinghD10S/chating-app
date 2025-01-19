e3a1fa0eb1a249c04a62c9c09cfe134fc9b570d8
ankitsingh <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-01-19 18:19:46 +0530

    fix: improve signup validation and add profile picture assignment in auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,39 +1,55 @@
 import User from "../models/user.model";
 
- const login = (req,res)=>{
+const login = (req, res) => {
     res.send("login phjxdfhjsage");
 };
 
- const signup =async (req,res)=>{
+const signup = async (req, res) => {
     try {
-        const {fullname ,username,password , confirmpassword,gender }=req.body
+        const { fullname, username, password, confirmpassword, gender } =
+            req.body;
 
-        if(password!==confirmpassword){
-            return res.status(400).json({error:"password not matching"});
+        if (password !== confirmpassword) {
+            return res.status(400).json({ error: "password not matching" });
         }
 
-        const user = await User.findOne({username})
+        const user = await User.findOne({ username });
 
-        if((user)){
-            res.status(400).json({error:"use ralreay exist"})
+        if (user) {
+            res.status(400).json({ error: "use ralreay exist" });
         }
 
-        
+        // hash password
 
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
+        const newUser = new User({
+            fullname,
+            username,
+            password,
+            gender,
+            profilePic: gender == male ? boyProfilePic : girlProfilePic,
+        });
 
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullname: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic,
+        });
     } catch (error) {
+        console.log(error);
         
+        res.status(500).json({ error: "internal server error" });
+
     }
-}
+};
 
- const logout = (req,res)=>{
-    res.send ("logout page");
-}
+const logout = (req, res) => {
+    res.send("logout page");
+};
 
-export {login,signup,logout};
-
-
-
-
-
+export { login, signup, logout };
