80841935a62b408444278118fa45b4bc3b4f2641
ankitsingh <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-01-25 23:50:38 +0530

    feat: add getMessage controller and update message routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/message.routes.js"
 import dbConnect from "./db/dbConnect.js";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js"
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes)
 
 app.listen(PORT, () => {
     dbConnect();
