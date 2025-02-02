85430b62c95b4eddb924014a2b261d8af04d8ed0
ankitsingh <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-02-02 14:49:53 +0530

    feat: serve frontend static files and add build script for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js"
 const PORT = process.env.PORT || 5000;
 import {app ,server} from "./socket/socket.js"
+import path from "path"
 dotenv.config();
+
+const __dirname = path.resolve();
 
 app.use(express.json()) //to parse the incoming  requets with Json payloads (from req.body)
 app.use(cookieParser())
@@ -15,6 +18,12 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT, () => {
     dbConnect();
